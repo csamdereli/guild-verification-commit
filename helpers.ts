8fb055d17165177a85148c66634f8395b3ded1b9
csamdereli <156028807+csamdereli@users.noreply.github.com>
2026-03-06 15:31:02 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:30:22.961Z
-Commit: Add dark mode support
+Timestamp: 2026-03-06T12:31:01.502Z
+Commit: Resolve null pointer exception in user service
