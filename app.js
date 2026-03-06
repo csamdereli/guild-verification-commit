8a35308a179b07c523d7bd67a560e2a645ab60d3
csamdereli <156028807+csamdereli@users.noreply.github.com>
2026-03-06 15:27:52 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:27:49.697Z
-Commit: Add multi-language support
+Timestamp: 2026-03-06T12:27:51.173Z
+Commit: Resolve timezone issues in date handling
