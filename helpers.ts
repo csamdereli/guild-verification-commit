a241f780f82da29ddf90dbf9f0e266444f313b4d
csamdereli <156028807+csamdereli@users.noreply.github.com>
2026-03-06 15:31:12 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:31:01.502Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T12:31:11.508Z
+Commit: Add dark mode support
