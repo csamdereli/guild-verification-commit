886b3b624e3ceaa401a5de0dee0fef8ae290cd0d
csamdereli <156028807+csamdereli@users.noreply.github.com>
2026-03-06 15:31:36 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:28:22.439Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T12:31:34.870Z
+Commit: Patch API endpoint error handling
